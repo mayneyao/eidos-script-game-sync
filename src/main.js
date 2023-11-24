cb97ae34c5714723b9a74ba8b2e5039fc67cf0bf
Mayne <i@gine.me>
2023-11-24 09:32:20 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 /// <reference path="eidos.d.ts" />
 export default async function (input, context) {
-    console.log("Hello Eidos!");
-    const tableName = context.tables.todo.name;
-    const fieldMap = context.tables.todo.fieldsMap;
-    const res = await eidos.currentSpace.addRow(tableName, {
-        [fieldMap.title]: input.content,
-    });
-    console.log(res);
+    const getAllMyGames = (userName) => {
+        let allGames = [];
+        let startUrl = `https://api.rawg.io/api/users/${userName}/games?page=1`;
+        const getGames = (url) => {
+            return fetch(url)
+                .then((res) => {
+                return res.json();
+            })
+                .then((data) => {
+                allGames = allGames.concat(data.results);
+                if (data.next) {
+                    return getGames(data.next);
+                }
+                else {
+                    return allGames;
+                }
+            });
+        };
+        return getGames(startUrl);
+    };
+    const games = await getAllMyGames(input.username);
+    const tableName = context.tables.game.name;
+    const fieldMap = context.tables.game.fieldsMap;
+    for (const game of games) {
+        const { name, slug, background_image, released } = game;
+        await eidos.currentSpace.addRow(tableName, {
+            [fieldMap.name]: name,
+            [fieldMap.slug]: slug,
+            [fieldMap.background_image]: background_image,
+            [fieldMap.released]: released,
+        });
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,21 @@ interface Env {
   // add your environment variables here
 }
 
+interface IGameItem {
+  name: string;
+  slug: string;
+  background_image: string;
+  released: string;
+}
+
 interface Table {
   // add your tables here
-  todo: EidosTable<{
-    title: string;
-  }>;
+  game: EidosTable<IGameItem>;
 }
 
 interface Input {
   // add your input fields here
-  content: string;
+  username: string;
 }
 
 interface Context {
@@ -23,11 +28,37 @@ interface Context {
 }
 
 export default async function (input: Input, context: Context) {
-  console.log("Hello Eidos!");
-  const tableName = context.tables.todo.name;
-  const fieldMap = context.tables.todo.fieldsMap;
-  const res = await eidos.currentSpace.addRow(tableName, {
-    [fieldMap.title]: input.content,
-  });
-  console.log(res);
+  const getAllMyGames = (userName: string) => {
+    let allGames: IGameItem[] = [];
+    let startUrl = `https://api.rawg.io/api/users/${userName}/games?page=1`;
+    const getGames = (url: string): Promise<IGameItem[]> => {
+      return fetch(url)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          allGames = allGames.concat(data.results);
+          if (data.next) {
+            return getGames(data.next);
+          } else {
+            return allGames;
+          }
+        });
+    };
+    return getGames(startUrl);
+  };
+
+  const games = await getAllMyGames(input.username);
+
+  const tableName = context.tables.game.name;
+  const fieldMap = context.tables.game.fieldsMap;
+  for (const game of games) {
+    const { name, slug, background_image, released } = game;
+    await eidos.currentSpace.addRow(tableName, {
+      [fieldMap.name]: name,
+      [fieldMap.slug]: slug,
+      [fieldMap.background_image]: background_image,
+      [fieldMap.released]: released,
+    });
+  }
 }
